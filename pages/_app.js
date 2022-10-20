76fa79f11078cfa347f64431e5569dd7e20c6798
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2022-10-20 16:18:59 -0500

    supabase is in the house

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { useState } from 'react'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+  return (
+
+
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionContextProvider>
+
+  )
 }
 
 export default MyApp
