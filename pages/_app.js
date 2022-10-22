2725b84167ce6a79bc30edf73c4872b5fce00574
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2022-10-21 23:24:26 -0500

    next-auth config

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,12 @@
-import { useState } from 'react'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { SessionProvider } from 'next-auth/react';
 
 
 function MyApp({ Component, pageProps }) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
   return (
-
-
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
-
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
-
-    </SessionContextProvider>
-
+    </SessionProvider>
   )
 }
 
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,37 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import FacebookProvider from "next-auth/providers/facebook"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      authorize: async (credentials) => {
+        const user = { id: 1, name: 'J Smith', email: '' }
+        if (credentials.username === user.name && credentials.password === user.password) {
+          const { password, ...userWithoutPassword } = user
+          return userWithoutPassword
+        } else {
+          return null
+        }
+      }
+    })
+  ],
+}
+
+export default NextAuth(authOptions)
